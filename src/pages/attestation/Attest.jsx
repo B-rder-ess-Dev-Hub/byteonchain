1464b6aa1f75c2271473bff152c6210f816b430b
primidac <primidac@gmail.com>
2025-01-31 12:02:45 +0100

    Attest Test

--- src/pages/attestation/Attest.jsx
+++ src/pages/attestation/Attest.jsx
@@ -1,26 +1,18 @@
 import { useState, useEffect } from "react";
 import { EAS, SchemaEncoder } from "@ethereum-attestation-service/eas-sdk";
-import { useAccount, useSigner } from "wagmi";
+import { useAccount } from "wagmi";
 import { useConnectModal } from "@rainbow-me/rainbowkit";
-<<<<<<< HEAD
-=======
 import { useSigner } from "./utils/wagmi-utils";
 import styles from "../../styles/Classroom.module.css";
 import Sidebar from "../../components/Sidebar";
 import Header from "../../components/Header";
->>>>>>> 709d318face457d143177728d9e12811edcf643b
 // import { useSearchParams } from "react-router-dom";
 
 const Attest = () => {
   const { isConnected } = useAccount();
   const { openConnectModal } = useConnectModal();
-<<<<<<< HEAD
-  const { data: signer } = useSigner();
-  const [searchParams] = useSearchParams();
-=======
   const signer = useSigner();
   // const [searchParams] = useSearchParams();
->>>>>>> 709d318face457d143177728d9e12811edcf643b
 
   const [attest, setAttest] = useState("");
   const [loading, setLoading] = useState(false);
@@ -44,9 +36,9 @@ const Attest = () => {
       );
 
       const encodedData = schemaEncoder.encodeData([
-        { name: "Name", value: "Primidac", type: "string" },
-        { name: "Course", value: "Cyber Security", type: "string" },
-        { name: "Score", value: 80 || "0", type: "uint256" },
+        { name: "Name", value: "Karla", type: "string" },
+        { name: "Course", value: "Business Development", type: "string" },
+        { name: "Score", value: 100 || "0", type: "uint256" },
         { name: "Issuer", value: "Borderless Community", type: "string" },
       ]);
 
@@ -61,78 +53,21 @@ const Attest = () => {
       });
 
       const newAttestationUID = await tx.wait();
+      if (newAttestationUID) {
+        window.open(
+          `https://sepolia.easscan.org/attestation/view/${newAttestationUID}`
+        );
+      }
       console.log("New attestation UID:", newAttestationUID);
     }
   }
   useEffect(() => {
-<<<<<<< HEAD
-    const addressParam = searchParams.get("address");
-    if (addressParam) setAddress(addressParam);
-
-    const issuerParam = searchParams.get("issuer");
-    if (issuerParam) setIssuerName(issuerParam);
-
-    const nameParam = searchParams.get("name");
-    if (nameParam) setName(nameParam);
-  }, [searchParams]);
-
-  useEffect(() => {
-    async function checkENS() {
-      if (address.includes(".eth")) {
-        const tmpAddress = await getAddressForENS(address);
-        setEnsResolvedAddress(tmpAddress || "");
-      } else {
-        setEnsResolvedAddress("");
-      }
-    }
-    checkENS();
-  }, [address]);
-
-  useEffect(() => {
-    async function connectEAS() {
-      const easContractAddress = "0xbD75f629A22Dc1ceD33dDA0b68c546A1c035c458";
-      const schemaUID = "0xe9a059a2f6cf3a119b0f8c1d5dc022711a447c7fbb94baf6ce670ca2e6faeaeb";
-      const eas = new EAS(easContractAddress);
-
-      if (signer) {
-        await eas.connect(signer);
-        const schemaEncoder = new SchemaEncoder(
-          "string Name,string Course,uint256 Score,string Issuer"
-        );
-
-        const encodedData = schemaEncoder.encodeData([
-          { name: "Name", value: name, type: "string" },
-          { name: "Course", value: title, type: "string" },
-          { name: "Score", value: score || "0", type: "uint256" },
-          { name: "Issuer", value: issuer, type: "string" },
-        ]);
-
-        const tx = await eas.attest({
-          schema: schemaUID,
-          data: {
-            recipient: "0x0000000000000000000000000000000000000000",
-            expirationTime: 0,
-            revocable: true, 
-            data: encodedData,
-          },
-        });
-
-        const newAttestationUID = await tx.wait();
-        console.log("New attestation UID:", newAttestationUID);
-      }
-    }
-=======
     async function connectEAS() {}
->>>>>>> 709d318face457d143177728d9e12811edcf643b
 
     if (signer) connectEAS();
   });
 
   return (
-<<<<<<< HEAD
-    <div>
-      <h3>Connect Your Wallet</h3>
-=======
     <div className={styles.container}>
       <Header />
       <div className={styles.layout}>
@@ -150,18 +85,20 @@ const Attest = () => {
         </div>
       </div>
 
->>>>>>> 709d318face457d143177728d9e12811edcf643b
       {!isConnected && (
-        <button onClick={handleConnectWallet}>Connect Wallet</button>
+        <div className={styles.walletOverlay}>
+          <div className={styles.walletContent}>
+            <h3>Connect Your Wallet</h3>
+            <p>Please connect your wallet to access the platform</p>
+            <button
+              className={styles.connectWalletButton}
+              onClick={handleConnectWallet}
+            >
+              Connect Wallet
+            </button>
+          </div>
+        </div>
       )}
-<<<<<<< HEAD
-      <input placeholder="Address/ENS" value={address} onChange={(e) => setAddress(e.target.value)} />
-      {ensResolvedAddress && <img src="/ens-logo.png" alt="ENS Logo" />}
-      <input placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
-      <input placeholder="Course Title" value={title} onChange={(e) => setTitle(e.target.value)} />
-      <input placeholder="Score" value={score} onChange={(e) => setScore(e.target.value)} />
-      <input placeholder="Issuer Name" value={issuer} onChange={(e) => setIssuerName(e.target.value)} />
-=======
 
       <div
         className={styles.buttonContainer}
@@ -173,7 +110,6 @@ const Attest = () => {
           Attest
         </button>
       </div>
->>>>>>> 709d318face457d143177728d9e12811edcf643b
     </div>
   );
 };
